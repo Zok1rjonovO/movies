c3ceb8cc582ce042775655bb9649f8393a2726d0
Olimjon Zokirjonov <olimjonzokirjonov@Olimjons-MacBook-Air.local>
2023-03-28 07:20:28 +0500

    js , html

--- js/script.js
+++ js/script.js
@@ -1,32 +1,110 @@
-let elList = selectEl("#list")
+let elForm = select("#form");
+let elGenreSelect = select("#genreSelect");
+let elList = select("#list");
+let cardTemplate = select("#card-template").content;
+let modalTemplate = select("#modal-template").content;
 
-function renderFunc(array , element) {
-
-    array.forEach(film => {
-        let newLi  = creatEl("li");
-        let newimg = creatEl("img");
-        let newh2 = creatEl("h2");
-        let newP = creatEl("p");
-        let newGenrelist = creatEl("ul");
-        let newBtn = creatEl("button")
-        
-        setAtt(newimg, "src" , film.Poster);
-        setAtt(newimg, "style" , "width:400;");
-        setAtt(newBtn, "type" , "button");
-        
-        newh2.textContent = film.Title
-        newP.textContent = "genres:"
-        newBtn.textContent = "Show more"
-
-        film.genres.forEach((genre) => {
-            let newGenreli = creatEl("li");
-            newGenreli.textContent = genre;
-            newGenrelist.append(newGenreli);
+elForm.addEventListener("submit",evt => {
+    evt.preventDefault()
+    
+    let {search, genre, sortBy} = evt.target.elements
+    
+    let regex = new RegExp(search.value.trim(), "gi");
+    
+    let searchedFilms = films.filter((film) => film.Title.match(regex));
+    console.log(searchedFilms);
+    if(genre.value != "all"){
+        let filteredByGenre = searchedFilms.filter((film) =>
+        film.genres.includes(genre.value)
+        );
+        searchedFilms = filteredByGenre;
+    }
+    
+    if(sortBy.value == "a-z"){
+        searchedFilms.sort((a, b)=> {
+            if(a.Title > b.Title){
+                return 1
+            }else if(a.Title < b.Title){
+                return -1
+            }else{
+                return 0
+            }
+        })
+    }else if (sortBy.value == "z-a"){
+        searchedFilms.sort((a, b) => {
+            if (b.Title > a.Title) {
+                return 1;
+            } else if (b.Title < a.Title) {
+                return -1;
+            } else {
+                return 0;
+            }
         });
+    }
+    renderFunc(searchedFilms, elList);
+})
 
-        newLi.append(newimg , newh2 , newP , newGenrelist , newBtn);
-
-        element.append(newLi);
+function renderGenre(array, element){
+    let genereArr = [];
+    
+    array.forEach((film) => {
+        film.genres.forEach(genre => {
+            !genereArr.includes(genre) ? genereArr.push(genre) : null;
+        })
+    });
+    
+    genereArr.forEach((genre) => {
+        let newOption = create("Option");
+        newOption.textContent = genre;
+        newOption.value = genre;
+        
+        element.append(newOption);
     });
 }
-renderFunc(films , elList)
+renderGenre(films, elGenreSelect);
+
+
+function renderFunc(array, element){
+    element.innerHTML = null;
+    array.forEach(film => {
+        let cloneTemplate = cardTemplate.cloneNode(true);
+        
+        let li = select("li", cloneTemplate);
+        let img = select("img", cloneTemplate);
+        let h2 = select("h2", cloneTemplate);
+        let btn = select("button", cloneTemplate);
+        
+        img.src = film.Poster;
+        h2.textContent = film.Title;
+        btn.dataset.id = film.id;
+        
+        btn.addEventListener("click", (evt)=> {
+            let filmId = evt.target.dataset.id;
+            let cloneTemplateModal = modalTemplate.cloneNode(true);
+            
+            let foundFilm = array.find((item) => item.id == filmId);
+            
+            let modal = select("#modal", cloneTemplateModal);
+            let iframe = select("iframe", cloneTemplateModal);
+            let h2 = select("h2", cloneTemplateModal);
+            let h3 = select("h3", cloneTemplateModal);
+            let p = select("p", cloneTemplateModal);
+            
+            iframe.src = foundFilm.link;
+            h2.textContent = foundFilm.Title;
+            h3.textContent = `Genres: ${foundFilm.genres.join(", ")}`;
+            p.textContent = foundFilm.overview;
+            
+            document.querySelector("body").append(modal);
+        })
+        
+        
+        element.append(li)
+    })
+}
+renderFunc(films, elList);
+
+function deleteModal(){
+    const elModal = document.getElementById("modal");
+    elModal.remove();
+}
